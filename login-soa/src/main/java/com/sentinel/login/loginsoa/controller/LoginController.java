dfc48b3ce12905f477d588dbd1ec529596a3e57f
liushuai <shuai9914nds@163.com>
2020-05-17 11:57:29 +0800

    新增UserInfo的mapper

---
+++ login-soa/src/main/java/com/sentinel/login/loginsoa/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.sentinel.login.loginsoa.controller;
+
+/**
+ * @author: liushuai
+ * @date: 2020/5/16
+ * @description：登录controller
+ */
+public class LoginController {
+}
---
+++ login-soa/src/main/java/com/sentinel/login/loginsoa/service/UserInfoService.java
@@ -0,0 +1,17 @@
+package com.sentinel.login.loginsoa.service;
+
+
+import com.baomidou.mybatisplus.service.IService;
+
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 刘帅
+ * @since 2020-05-17
+ */
+public interface UserInfoService  {
+	
+}
