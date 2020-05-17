afe78ddff415b1fae5cd7ec169b305747c899133
liushuai <shuai9914nds@163.com>
2020-05-17 14:25:41 +0800

    新增LoginController

--- login-soa/src/main/java/com/sentinel/login/loginsoa/controller/LoginController.java
+++ login-soa/src/main/java/com/sentinel/login/loginsoa/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.sentinel.login.loginsoa.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author: liushuai
  * @date: 2020/5/16
  * @description：登录controller
  */
+@RestController
 public class LoginController {
 }
