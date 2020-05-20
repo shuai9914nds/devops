056b9afa206611b503a8b1e9e6ce276233daeeb1
liushuai <shuai9914nds@163.com>
2020-05-20 11:06:56 +0800

    加入lombok

--- sentinel-login/src/main/java/com/sentinel/login/loginsoa/dto/UserInfoDto.java
+++ sentinel-login/src/main/java/com/sentinel/login/loginsoa/dto/UserInfoDto.java
@@ -1,5 +1,7 @@
 package com.sentinel.login.loginsoa.dto;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -8,6 +10,7 @@ import java.util.Date;
  * @date: 2020/5/19
  * @description：用户信息dto
  */
+@Data
 public class UserInfoDto implements Serializable {
     /**
      * 主键id
@@ -48,69 +51,4 @@ public class UserInfoDto implements Serializable {
      * 更新人
      */
     private String updateBy;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
 }
--- sentinel-login/src/main/java/com/sentinel/login/loginsoa/model/UserInfo.java
+++ sentinel-login/src/main/java/com/sentinel/login/loginsoa/model/UserInfo.java
@@ -1,7 +1,10 @@
 package com.sentinel.login.loginsoa.model;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class UserInfo {
     /**
      * 主键id
@@ -47,76 +50,4 @@ public class UserInfo {
      * 更新人
      */
     private String updateBy;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
 }
