64ab8a5f99c30b4c17d81817510018b8150c3520
liushuai <shuai9914nds@163.com>
2020-05-26 11:03:05 +0800

    新增role,permission,user_role的mapper

---
+++ sentinel-login/src/main/java/com/sentinel/login/loginsoa/mapper/SysPermissionMapper.java
@@ -0,0 +1,31 @@
+package com.sentinel.login.loginsoa.mapper;
+
+import com.sentinel.login.loginsoa.model.SysPermission;
+import com.sentinel.login.loginsoa.model.SysPermissionExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SysPermissionMapper {
+    int countByExample(SysPermissionExample example);
+
+    int deleteByExample(SysPermissionExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysPermission record);
+
+    int insertSelective(SysPermission record);
+
+    List<SysPermission> selectByExample(SysPermissionExample example);
+
+    SysPermission selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") SysPermission record, @Param("example") SysPermissionExample example);
+
+    int updateByExample(@Param("record") SysPermission record, @Param("example") SysPermissionExample example);
+
+    int updateByPrimaryKeySelective(SysPermission record);
+
+    int updateByPrimaryKey(SysPermission record);
+}
---
+++ sentinel-login/src/main/java/com/sentinel/login/loginsoa/model/RolePermission.java
@@ -0,0 +1,85 @@
+package com.sentinel.login.loginsoa.model;
+
+import java.util.Date;
+
+public class RolePermission {
+    private Integer id;
+
+    private Integer roleId;
+
+    private Integer permissionId;
+
+    private Date createTime;
+
+    private String createBy;
+
+    private Date updateTime;
+
+    private String updateBy;
+
+    private Boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Integer permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
