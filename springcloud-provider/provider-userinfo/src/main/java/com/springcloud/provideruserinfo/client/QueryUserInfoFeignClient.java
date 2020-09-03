3757e00cab9f7ceba87d7e43407edc3d1deed39b
liushuai <shuai9914nds@163.com>
2020-09-03 15:11:45 +0800

    搭建springcloud项目

--- springcloud-provider/provider-userinfo/src/main/java/com/springcloud/provideruserinfo/client/QueryUserInfoFeignClient.java
+++ springcloud-provider/provider-userinfo/src/main/java/com/springcloud/provideruserinfo/client/QueryUserInfoFeignClient.java
@@ -3,6 +3,7 @@ package com.springcloud.provideruserinfo.client;
 import com.springcloud.userinfoapi.model.UserInfo;
 import com.springcloud.userinfoapi.service.QueryUserInfoFeignApi;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,14 @@ import java.util.List;
 @RestController
 @Api(value = "API - QueryUserInfoFeignClient", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class QueryUserInfoFeignClient implements QueryUserInfoFeignApi {
+
+    /**
+     * 查询用户信息列表
+     * @param userInfo
+     * @return
+     */
     @Override
+    @ApiOperation(httpMethod = "POST", value = "查询用户信息")
     public List<UserInfo> getUserInfoByCondition(UserInfo userInfo) {
         return null;
     }
