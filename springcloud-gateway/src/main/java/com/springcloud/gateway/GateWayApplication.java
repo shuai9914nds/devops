cbced90bd95dd5c79de9a083c3a2e6c774df2803
liushuai <shuai9914nds@163.com>
2020-09-15 19:33:41 +0800

    增加login模块

--- springcloud-gateway/src/main/java/com/springcloud/gateway/GateWayApplication.java
+++ springcloud-gateway/src/main/java/com/springcloud/gateway/GateWayApplication.java
@@ -1,10 +1,7 @@
-package com.springcloud.gateway.login;
+package com.springcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GateWayApplication {
---
+++ springcloud-login/src/main/java/com/springcloud/login/SpringcloudLoginApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudLoginApplication.class, args);
+    }
+
+}
