bd5c861ad8c5705743be86bad724328e990b9050
liushuai <shuai9914nds@163.com>
2020-11-19 15:26:21 +0800

    1、去掉注释

--- vue-web/src/components/Login.vue
+++ vue-web/src/components/Login.vue
@@ -37,42 +37,13 @@
 
                 <div class="field" style="margin-top: 10px">
                     <p class="control">
-                        <!--                        <button @click="login" type="submit" class="button">-->
-                        <!--                            登录-->
-                        <!--                        </button>-->
                         <a-button type="Default" @click="login">登录</a-button>
-
                     </p>
                 </div>
             </div>
         </el-form>
     </div>
 </template>
-<!--<template>-->
-<!--    <div>-->
-<!--        <a-button type="primary">-->
-<!--            Primary-->
-<!--        </a-button>-->
-<!--        <a-button>Default</a-button>-->
-<!--        <a-button type="dashed">-->
-<!--            Dashed-->
-<!--        </a-button>-->
-<!--        <a-button type="danger">-->
-<!--            Danger-->
-<!--        </a-button>-->
-<!--        <a-config-provider :auto-insert-space-in-button="false">-->
-<!--            <a-button type="primary">-->
-<!--                按钮-->
-<!--            </a-button>-->
-<!--        </a-config-provider>-->
-<!--        <a-button type="primary">-->
-<!--            按钮-->
-<!--        </a-button>-->
-<!--        <a-button type="link">-->
-<!--            Link-->
-<!--        </a-button>-->
-<!--    </div>-->
-<!--</template>-->
 
 <script>
 
