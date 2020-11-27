919f9d5a40b3b64da7a115831681dcc71f02c791
liushuai <shuai9914nds@163.com>
2020-11-27 20:10:54 +0800

    增加更新用户状态接口

---
+++ springcloud-user/src/main/java/com/springcloud/user/client/query/QueryUserClient.java
@@ -0,0 +1,128 @@
+package com.springcloud.user.client;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.springcloud.user.entity.UserInfo;
+import com.springcloud.user.service.IUserInfoService;
+import com.springcloud.user.util.JwtUtil;
+import com.user.api.dto.UserInfoDto;
+import common.Constant;
+import common.Result;
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import utils.BeanConverter;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author: liushuai
+ * @date: 2020/10/3
+ * @description：
+ */
+@RestController
+@Api(value = "API - QueryUserClient")
+public class QueryUserClient {
+    private static final Logger logger = LoggerFactory.getLogger(QueryUserClient.class);
+    @Resource
+    private IUserInfoService iUserInfoService;
+
+    /**
+     * 根据用户名查询用户信息 (包含密码，只允许内部调用)
+     *
+     * @param username 用户名
+     * @return Result<UserInfoDto>
+     */
+    @GetMapping(value = "/{username}/user")
+    public Result<UserInfo> getUserByUserName(@PathVariable("username") String username) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername(username);
+        return new Result<>(iUserInfoService.getOneByCondition(userInfo));
+    }
+
+    /**
+     * 查询全部用户
+     *
+     * @param
+     * @return
+     */
+    @GetMapping(value = "/user/list")
+    public Result<List<UserInfoDto>> getUserAll() {
+        List<UserInfo> userInfos = iUserInfoService.getBaseMapper().selectList(null);
+        return new Result<>(BeanConverter.convertList(userInfos, UserInfoDto.class));
+    }
+
+    /**
+     * 分页查询用户信息
+     *
+     * @param current 当前页
+     * @param size    每页显示行数
+     * @param name    用户名称
+     * @return Result<IPage < UserInfo>>
+     */
+    @GetMapping(value = "/user/page")
+    public Result<Page<UserInfo>> selectUserPage(@RequestParam("current") Long current, @RequestParam("size") Long size,
+                                                 @RequestParam(value = "name", required = false) String name) {
+        Page<UserInfo> page = new Page<>();
+        page.setCurrent(current);
+        page.setSize(size);
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName(name);
+        Page<UserInfo> userInfoIPage = iUserInfoService.selectUserPage(page, userInfo);
+        List<UserInfo> records = userInfoIPage.getRecords();
+        if (CollectionUtils.isEmpty(records)) {
+            return new Result<>(userInfoIPage);
+        }
+        records.forEach(i -> {
+            i.setPassword("");
+        });
+        return new Result<>(userInfoIPage);
+    }
+
+    /**
+     * 创建token
+     *
+     * @param uid  用户id
+     * @param name 用户名称
+     * @return Result<String>
+     */
+    @GetMapping(value = "/create/token")
+    public Result<String> createToken(@RequestParam("uid") Integer uid, @RequestParam("name") String name) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUid(uid);
+        userInfo.setName(name);
+        return new Result<>(JwtUtil.getToken(userInfo));
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @param token 用户的token
+     * @return UserInfo
+     */
+    @GetMapping(value = "/token/user")
+    public UserInfo getUserByToken(String token) {
+        DecodedJWT decode = JWT.decode(token);
+        Integer userId = decode.getClaim(Constant.DEVOPS_USER_ID).asInt();
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUid(userId);
+        return iUserInfoService.getOneByCondition(userInfo);
+    }
+
+    /**
+     * 获取token
+     *
+     * @return token
+     */
+    public String getToken() {
+
+        return iUserInfoService.getToken();
+    }
+}
--- vue-web/src/views/user/User.vue
+++ vue-web/src/views/user/User.vue
@@ -5,11 +5,9 @@
         <a-input v-model="this.name" />
       </a-form-item>
 
-    <a-config-provider :auto-insert-space-in-button="false">
-      <a-button type="primary" html-type="submit">
-        查询
-      </a-button>
-    </a-config-provider>
+      <a-config-provider :auto-insert-space-in-button="false">
+        <a-button type="primary" html-type="submit"> 查询 </a-button>
+      </a-config-provider>
     </a-form>
 
     <a-table
@@ -25,9 +23,49 @@
         <span v-if="record.state == 1">锁定</span>
         <span v-if="record.state == 2">禁用</span>
       </template>
-
+      <!-- <template slot="name" slot-scope="text, record">
+        <editable-cell
+          :text="text"
+          @change="onCellChange(record.uid, 'name', $event)"
+        />
+      </template> -->
       <template slot="action" slot-scope="text, record">
-        <span>删除</span>
+        <!-- <editable-cell
+          :text="text"
+          @change="onCellChange(record.key, 'action', $event)"
+        /> -->
+        <span v-if="record.state == 0">
+          <span>
+            <a-popconfirm
+              v-if="data.length"
+              title="是否锁定该用户?"
+              @confirm="() => onDelete(record.uid)"
+            >
+              <a href="javascript:;">锁定</a>
+            </a-popconfirm>
+
+            <a-divider type="vertical" />
+          </span>
+          <span>
+            <!-- <a-popconfirm
+              title="Sure to delete?"
+              @confirm="() => onDelete(record.key)"
+            >
+            </a-popconfirm> -->
+            <a href="javascript:;">禁用</a>
+          </span>
+        </span>
+        <span v-if="record.state == 1">
+          <a>恢复正常</a>
+          <a-divider type="vertical" />
+          <a>禁用</a>
+        </span>
+
+        <span v-if="record.state == 2">
+          <a>恢复正常</a>
+          <a-divider type="vertical" />
+          <a>锁定</a>
+        </span>
       </template>
     </a-table>
   </div>
@@ -42,9 +80,10 @@ const columns = [
     title: "用户名",
     dataIndex: "username",
   },
-    {
+  {
     title: "姓名",
     dataIndex: "name",
+    scopedSlots: { customRender: "name" },
   },
   {
     title: "身份证号",
@@ -79,6 +118,46 @@ const columns = [
   //   },
 ];
 
+const EditableCell = {
+  template: `
+      <div class="editable-cell">
+        <div v-if="editable" class="editable-cell-input-wrapper">
+          <a-input :value="value" @change="handleChange" @pressEnter="check" /><a-icon
+            type="check"
+            class="editable-cell-icon-check"
+            @click="check"
+          />
+        </div>
+        <div v-else class="editable-cell-text-wrapper">
+          {{ value || ' ' }}
+          <a-icon type="edit" class="editable-cell-icon" @click="edit" />
+        </div>
+      </div>
+    `,
+  props: {
+    text: String,
+  },
+  data() {
+    return {
+      value: this.text,
+      editable: false,
+    };
+  },
+  methods: {
+    handleChange(e) {
+      const value = e.target.value;
+      this.value = value;
+    },
+    check() {
+      this.editable = false;
+      this.$emit("change", this.value);
+    },
+    edit() {
+      this.editable = true;
+    },
+  },
+};
+
 export default {
   data() {
     return {
@@ -108,11 +187,11 @@ export default {
     },
     selectUserPage(params = {}) {
       this.$axios
-        .get("/user/page/v1", {
+        .get("/user/page", {
           params: {
             size: 10,
             current: params.current === undefined ? 1 : params.current,
-            name:this.name,
+            name: this.name,
             ...params,
           },
           headers: {
@@ -125,9 +204,48 @@ export default {
           this.data = response.data.obj.records;
           pagination.total = page.total;
           pagination.pageSize = page.size;
-          console.log(pagination)
+          console.log(pagination);
           this.loading = false;
           this.pagination = pagination;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    onCellChange(key, dataIndex, value) {
+      const dataSource = [...this.dataSource];
+      const target = dataSource.find((item) => item.key === key);
+      if (target) {
+        target[dataIndex] = value;
+        this.dataSource = dataSource;
+      }
+    },
+    onDelete(uid) {
+      const dataSource = [...this.data];
+      const params = dataSource.filter((item) => item.uid == uid);
+      this.updateUserState({
+        uid: params[0].uid,
+        state: params[0].state,
+      });
+    },
+    updateUserState(params = {}) {
+      this.$axios
+        .post(
+          "/user/state",
+          {
+            uid: params.uid,
+            state: params.state,
+          },
+          {
+            headers: {
+              // "content-type": "application/json",
+              "User-Token": localStorage.getItem("token"),
+            },
+          }
+        )
+        .then((response) => {
+          this.data = response.data.obj.records;
+          this.selectUserPage();
         })
         .catch((error) => {
           console.log(error);
@@ -136,3 +254,48 @@ export default {
   },
 };
 </script>
+
+<style>
+.editable-cell {
+  position: relative;
+}
+
+.editable-cell-input-wrapper,
+.editable-cell-text-wrapper {
+  padding-right: 24px;
+}
+
+.editable-cell-text-wrapper {
+  padding: 5px 24px 5px 5px;
+}
+
+.editable-cell-icon,
+.editable-cell-icon-check {
+  position: absolute;
+  right: 0;
+  width: 20px;
+  cursor: pointer;
+}
+
+.editable-cell-icon {
+  line-height: 18px;
+  display: none;
+}
+
+.editable-cell-icon-check {
+  line-height: 28px;
+}
+
+.editable-cell:hover .editable-cell-icon {
+  display: inline-block;
+}
+
+.editable-cell-icon:hover,
+.editable-cell-icon-check:hover {
+  color: #108ee9;
+}
+
+.editable-add-btn {
+  margin-bottom: 8px;
+}
+</style>
