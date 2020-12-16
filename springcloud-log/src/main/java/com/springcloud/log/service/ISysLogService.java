13eb38c4f8177f9b108b66e2684d0de7d5c32dbb
liushuai <shuai9914nds@163.com>
2020-12-16 11:08:12 +0800

    生成syslog的mapper 增加分页查询日志接口

---
+++ springcloud-log/src/main/java/com/springcloud/log/service/ISysLogService.java
@@ -0,0 +1,16 @@
+package com.springcloud.log.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.springcloud.log.entity.SysLog;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author shuai
+ * @since 2020-12-16
+ */
+public interface ISysLogService extends IService<SysLog> {
+
+}
