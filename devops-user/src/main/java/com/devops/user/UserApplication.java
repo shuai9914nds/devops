03653dc44a01b083b1abcf76735ba8968ac0300a
liushuai <shuai9914nds@163.com>
2020-12-26 22:59:01 +0800

    解决引入ComponentScan 找不到feign问题

--- devops-user/src/main/java/com/devops/user/UserApplication.java
+++ devops-user/src/main/java/com/devops/user/UserApplication.java
@@ -4,10 +4,11 @@ import com.devops.base.annotation.MyApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients({"com.user.*"})
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.devops.base.aspect", "com.devops.base.annotation"})
+@ComponentScan(basePackages = {"com.devops.base.aspect", "com.devops.base.annotation", "com.devops.user.*"})
 @MyApplication
 public class UserApplication {
 
