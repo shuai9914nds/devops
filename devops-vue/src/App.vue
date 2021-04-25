f4d989cd6a31c73bce1a0408ed689ed093ec574b
liushuai <shuai9914nds@163.com>
2021-04-25 19:06:08 +0800

    将注册中心从eureka改为nacos

--- devops-vue/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="app1">
-    <router-view />
-  </div>
-</template>
-<script>
-export default {}
-</script>
--- devops-vue/src/main.js
+++ devops-vue/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './views/App.vue'
 import antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import router from './router'
